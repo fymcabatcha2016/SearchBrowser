21fc64360bfe80d2699efecaf781aba19eb83296
fymcabatcha2016 <fymcabatcha2016@gmail.com>
2018-08-28 09:35:55 +0530

    component and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchComponent } from './search/search.component';
+import { SearchTabComponent } from './search/search-tab/search-tab.component';
+import { SearchResultComponent } from './search/search-result/search-result.component';
+import { AdvertisementComponent } from './advertisement/advertisement.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchTabComponent,
+    SearchResultComponent,
+    AdvertisementComponent
   ],
   imports: [
     BrowserModule,
